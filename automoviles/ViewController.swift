e5524c3f1a7fbb1cc114074ecf7aaa136ae9e815
Ezequiel <ezequiel3434@gmail.com>
2020-07-17 19:42:51 -0300

    initial commit

---
+++ automoviles/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  automoviles
+//
+//  Created by Juan on 19/01/17.
+//  Copyright © 2017 Juan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var fotografiaAutomovil: UIImageView!
+    
+    
+    @IBOutlet weak var marcasSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var modeloLabel: UILabel!
+    
+    @IBOutlet weak var calificacionLabel: UILabel!
+    
+    @IBOutlet weak var numeroPruebasLabel: UILabel!
+    
+    @IBOutlet weak var ultimaPruebaLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func segmentedControl(_ sender: Any) {
+    }
+   
+    @IBAction func probar(_ sender: Any) {
+    }
+
+    @IBAction func calificar(_ sender: Any) {
+    }
+
+}
+
